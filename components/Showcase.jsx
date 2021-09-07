cc75de929a005e018895fa36558cf36920184ee0
just4speed <thestanpie@ya.ru>
2021-09-07 18:09:00 +0500

    Added MaskedView

--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -1,19 +1,41 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { View, Image } from 'react-native';
-import { MaskImageView } from 'react-native-mask-image';
+import MaskedView from '@react-native-masked-view/masked-view';
 
 const Showcase = () => {
     const main = useSelector(state => state.main);
+    // resizeMode='cover' 
     return(
         <View>
-            <Image style={{
-                width: 300,
-                height: 210,
-                marginBottom: 15,
-                resizeMode: 'contain',
-                alignSelf: 'center',
-            }} source={require('../outline.png')} />
+            <MaskedView
+                resizeMode={'cover'}
+                style={{ flex: 1 }}
+                maskElement={
+                    <Image
+                        style={{
+                            backgroundColor: main.fillColor,
+                            zIndex: 1,
+                            width: 300,
+                            height: 210,
+                            alignSelf: 'center',
+                            position: 'fixed'
+                        }}
+                        resizeMode={'cover'}
+                        source={require('../assets/upfill.png')}
+                    />
+                }
+            >
+                <Image style={{
+                    zIndex: 2,
+                    width: 300,
+                    height: 210,
+                    marginBottom: 15,
+                    resizeMode: 'contain',
+                    alignSelf: 'center',
+                    position: 'fixed'
+                }} source={require('../outline.png')} />
+            </MaskedView>
         </View>
     )
 }
